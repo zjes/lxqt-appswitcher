dce8e538ca16fc18c675b0220bb378a60bc9f7b9
stefonarch <stef.binde@riseup.net>
2016-09-28 21:40:59 +0200

    Created italian translation

---
+++ config/translations/lxqt-config-appswitcher_it.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>Config</name>
+    <message>
+        <location filename="../config.ui" line="14"/>
+        <source>LXQt Application Switcher Configuration</source>
+        <translation>Preferenze scambiafinestre </translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="20"/>
+        <source>Appearance:</source>
+        <translation>Aspetto:</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="26"/>
+        <source>Icons &amp;size</source>
+        <translation>Dimensione &amp;icone</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="36"/>
+        <source> px</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="43"/>
+        <source>Max &amp;items to show</source>
+        <translation>Numero &amp;massimo di voci da mostrare</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="60"/>
+        <source>Max text &amp;length</source>
+        <translation>Lunghezza &amp;massima</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="77"/>
+        <source>Custom &amp;font size</source>
+        <translation>Dimensione carattere &amp;personalizzato</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="87"/>
+        <source> pt</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="103"/>
+        <source>Filters:</source>
+        <translation>Filtri:</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="109"/>
+        <source>Filter by &amp;desktop</source>
+        <translation>Filtra per &amp;desktop</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="116"/>
+        <source>Filter by s&amp;creen</source>
+        <translation>Filtra per &amp;schermo</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/lxqt-appswitcher_it.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>AppSwitcher</name>
+    <message>
+        <location filename="../src/app-switcher.cpp" line="21"/>
+        <source>Switch applications</source>
+        <translation>Circola tra le finestre</translation>
+    </message>
+    <message>
+        <location filename="../src/app-switcher.cpp" line="22"/>
+        <source>Reverse switch applications</source>
+        <translation>Circola tra le finestre in direzione inversa</translation>
+    </message>
+</context>
+</TS>
