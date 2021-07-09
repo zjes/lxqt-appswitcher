9f3025ae3fc0040364febecd097c5beca61e9f49
Moo <hazap@hotmail.com>
2021-07-09 12:07:46 +0200

    Weblate commits
    Co-authored-by: Moo <hazap@hotmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswichter-configuration/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswitcher/lt/
    Translation: zjes/lxqt-appswitcher
    Translation: zjes/lxqt-appswitcher-configuration

---
+++ config/translations/lxqt-config-appswitcher_lt.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt">
+<context>
+    <name>Config</name>
+    <message>
+        <location filename="../config.ui" line="14"/>
+        <source>LXQt Application Switcher Configuration</source>
+        <translation>LXQt programų perjungiklio konfigūracija</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="20"/>
+        <source>Appearance:</source>
+        <translation>Išvaizda:</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="26"/>
+        <source>Icons &amp;size</source>
+        <translation>Piktogramų &amp;dydis</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="36"/>
+        <source> px</source>
+        <translation> piks.</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="43"/>
+        <source>Max &amp;items to show</source>
+        <translation>Daugiausiai &amp;rodyti elementų</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="60"/>
+        <source>Max text &amp;length</source>
+        <translation>Didžiausias teksto &amp;ilgis</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="77"/>
+        <source>Custom &amp;font size</source>
+        <translation>Tinkintas š&amp;rifto dydis</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="87"/>
+        <source> pt</source>
+        <translation> tšk.</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="103"/>
+        <source>Filters:</source>
+        <translation>Filtrai:</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="109"/>
+        <source>Filter by &amp;desktop</source>
+        <translation>Filtruoti pagal &amp;darbalaukį</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="116"/>
+        <source>Filter by s&amp;creen</source>
+        <translation>Filtruoti pagal e&amp;kraną</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/lxqt-appswitcher_lt.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt">
+<context>
+    <name>AppSwitcher</name>
+    <message>
+        <location filename="../src/app-switcher.cpp" line="21"/>
+        <source>Switch applications</source>
+        <translation>Perjungti programas</translation>
+    </message>
+    <message>
+        <location filename="../src/app-switcher.cpp" line="22"/>
+        <source>Reverse switch applications</source>
+        <translation>Atvirkščiai perjungti programas</translation>
+    </message>
+</context>
+</TS>
