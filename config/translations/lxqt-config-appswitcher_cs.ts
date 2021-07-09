68511d056888a0640b38d246c8fc50d154ddcf79
psimacek <petr.simacek@gmail.com>
2021-07-09 12:07:45 +0200

    Weblate commits
    Co-authored-by: psimacek <petr.simacek@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswichter-configuration/cs/
    Translation: zjes/lxqt-appswitcher-configuration

--- config/translations/lxqt-config-appswitcher_cs.ts
+++ config/translations/lxqt-config-appswitcher_cs.ts
@@ -6,57 +6,57 @@
     <message>
         <location filename="../config.ui" line="14"/>
         <source>LXQt Application Switcher Configuration</source>
-        <translation type="unfinished">LXQt Konfigurace přepínání aplikací</translation>
+        <translation>LXQt nastavení přepínání aplikací</translation>
     </message>
     <message>
         <location filename="../config.ui" line="20"/>
         <source>Appearance:</source>
-        <translation type="unfinished">Vzhled:</translation>
+        <translation>Vzhled:</translation>
     </message>
     <message>
         <location filename="../config.ui" line="26"/>
         <source>Icons &amp;size</source>
-        <translation type="unfinished">Velikost ikon</translation>
+        <translation type="unfinished">Velikost &amp;ikon</translation>
     </message>
     <message>
         <location filename="../config.ui" line="36"/>
         <source> px</source>
-        <translation type="unfinished"> px</translation>
+        <translation> px</translation>
     </message>
     <message>
         <location filename="../config.ui" line="43"/>
         <source>Max &amp;items to show</source>
-        <translation type="unfinished">Max položek k zobrazení</translation>
+        <translation type="unfinished">Maximum &amp;položek k zobrazení</translation>
     </message>
     <message>
         <location filename="../config.ui" line="60"/>
         <source>Max text &amp;length</source>
-        <translation type="unfinished">Max délka textu</translation>
+        <translation type="unfinished">Max &amp;délka textu</translation>
     </message>
     <message>
         <location filename="../config.ui" line="77"/>
         <source>Custom &amp;font size</source>
-        <translation type="unfinished">Výška písma</translation>
+        <translation>Velikost &amp;písma</translation>
     </message>
     <message>
         <location filename="../config.ui" line="87"/>
         <source> pt</source>
-        <translation type="unfinished"> pt</translation>
+        <translation> pt</translation>
     </message>
     <message>
         <location filename="../config.ui" line="109"/>
         <source>Filter by &amp;desktop</source>
-        <translation type="unfinished">Filtrovat podle plochy</translation>
+        <translation type="unfinished">Filtrovat podle &amp;plochy</translation>
     </message>
     <message>
         <location filename="../config.ui" line="116"/>
         <source>Filter by s&amp;creen</source>
-        <translation type="unfinished">Filtrovat podle obrazovky</translation>
+        <translation type="unfinished">Filtrovat podle o&amp;brazovky</translation>
     </message>
     <message>
         <location filename="../config.ui" line="103"/>
         <source>Filters:</source>
-        <translation type="unfinished">Filtry:</translation>
+        <translation>Filtry:</translation>
     </message>
 </context>
 </TS>
