dfa5d2443c5e20d96d111da449ae9ebaf5df1ebc
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-07-09 12:07:45 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswichter-configuration/et/
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswitcher/et/
    Translation: zjes/lxqt-appswitcher
    Translation: zjes/lxqt-appswitcher-configuration

---
+++ config/translations/lxqt-config-appswitcher_et.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>Config</name>
+    <message>
+        <location filename="../config.ui" line="14"/>
+        <source>LXQt Application Switcher Configuration</source>
+        <translation>LXQt rakenduste vahetaja seadistused</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="20"/>
+        <source>Appearance:</source>
+        <translation>Välimus:</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="26"/>
+        <source>Icons &amp;size</source>
+        <translation>Ikoonide &amp;suurus</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="36"/>
+        <source> px</source>
+        <translation> pikslit</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="43"/>
+        <source>Max &amp;items to show</source>
+        <translation>Suurim näidatavate k&amp;irjete arv</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="60"/>
+        <source>Max text &amp;length</source>
+        <translation>Pikim teksti &amp;pikkus</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="77"/>
+        <source>Custom &amp;font size</source>
+        <translation>Kohandatud &amp;fondisuurus</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="87"/>
+        <source> pt</source>
+        <translation> punkti</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="103"/>
+        <source>Filters:</source>
+        <translation>Filtrid:</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="109"/>
+        <source>Filter by &amp;desktop</source>
+        <translation>Näita &amp;töölaua alusel</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="116"/>
+        <source>Filter by s&amp;creen</source>
+        <translation>Näita &amp;ekraani järgi</translation>
+    </message>
+</context>
+</TS>
