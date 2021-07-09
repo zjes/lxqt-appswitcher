3559d8e54efe25470a513ed746dfe8979da9a089
Adolfo Jayme Barrientos <fitojb@ubuntu.com>
2021-07-09 12:07:45 +0200

    Weblate commits
    Co-authored-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswichter-configuration/es/
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswitcher/es/
    Translation: zjes/lxqt-appswitcher
    Translation: zjes/lxqt-appswitcher-configuration

---
+++ config/translations/lxqt-config-appswitcher_es.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>Config</name>
+    <message>
+        <location filename="../config.ui" line="14"/>
+        <source>LXQt Application Switcher Configuration</source>
+        <translation>Configuración del cambiador de aplicaciones de LXQt</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="20"/>
+        <source>Appearance:</source>
+        <translation>Apariencia:</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="26"/>
+        <source>Icons &amp;size</source>
+        <translation>&amp;Tamaño de iconos</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="36"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="43"/>
+        <source>Max &amp;items to show</source>
+        <translation>Máximo de &amp;elementos para mostrar</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="60"/>
+        <source>Max text &amp;length</source>
+        <translation>&amp;Longitud máxima de texto</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="77"/>
+        <source>Custom &amp;font size</source>
+        <translation>Tamaño de let&amp;ra personalizado</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="87"/>
+        <source> pt</source>
+        <translation> pt</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="103"/>
+        <source>Filters:</source>
+        <translation>Filtros:</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="109"/>
+        <source>Filter by &amp;desktop</source>
+        <translation>Filtrar por e&amp;scritorio</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="116"/>
+        <source>Filter by s&amp;creen</source>
+        <translation>Filtrar por pa&amp;ntalla</translation>
+    </message>
+</context>
+</TS>
