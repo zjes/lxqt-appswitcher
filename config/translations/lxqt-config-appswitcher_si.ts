87d5f1aadf1a45bb8bef01093749c0999f48fb12
helabasa <R45XvezA@pm.me>
2021-07-09 12:07:45 +0200

    Weblate commits
    Co-authored-by: helabasa <R45XvezA@pm.me>
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswichter-configuration/si/
    Translation: zjes/lxqt-appswitcher-configuration

---
+++ config/translations/lxqt-config-appswitcher_si.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="si">
+<context>
+    <name>Config</name>
+    <message>
+        <location filename="../config.ui" line="14"/>
+        <source>LXQt Application Switcher Configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="20"/>
+        <source>Appearance:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="26"/>
+        <source>Icons &amp;size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="36"/>
+        <source> px</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="43"/>
+        <source>Max &amp;items to show</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="60"/>
+        <source>Max text &amp;length</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="77"/>
+        <source>Custom &amp;font size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="87"/>
+        <source> pt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="103"/>
+        <source>Filters:</source>
+        <translation>පෙරහන්:</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="109"/>
+        <source>Filter by &amp;desktop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="116"/>
+        <source>Filter by s&amp;creen</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
