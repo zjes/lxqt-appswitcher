881b35e4d9d136b56c284d23db949e96ba58bb4d
Taras Panchenko <top39@ukr.net>
2021-09-08 20:08:03 +0200

    Weblate commits
    Co-authored-by: Taras Panchenko <top39@ukr.net>
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswichter-configuration/ru/
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswichter-configuration/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswitcher/uk/
    Translation: zjes/lxqt-appswitcher
    Translation: zjes/lxqt-appswitcher-configuration

--- config/translations/lxqt-config-appswitcher_uk.ts
+++ config/translations/lxqt-config-appswitcher_uk.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru">
+<TS version="2.1" language="uk">
 <context>
     <name>Config</name>
     <message>
         <location filename="../config.ui" line="14"/>
         <source>LXQt Application Switcher Configuration</source>
-        <translation>Настройки LXQt Application Switcher</translation>
+        <translation>Налаштування перемикання застосунків LXQT</translation>
     </message>
     <message>
         <location filename="../config.ui" line="20"/>
         <source>Appearance:</source>
-        <translation>Внешний вид:</translation>
+        <translation>Зовнішній вигляд:</translation>
     </message>
     <message>
         <location filename="../config.ui" line="26"/>
         <source>Icons &amp;size</source>
-        <translation>Размер значков</translation>
+        <translation>Розмір &amp;значків</translation>
     </message>
     <message>
         <location filename="../config.ui" line="36"/>
@@ -26,17 +26,17 @@
     <message>
         <location filename="../config.ui" line="43"/>
         <source>Max &amp;items to show</source>
-        <translation>Максимальное количество отображаемых элементов</translation>
+        <translation>Максимальна к&amp;ількість елементів для показу</translation>
     </message>
     <message>
         <location filename="../config.ui" line="60"/>
         <source>Max text &amp;length</source>
-        <translation>Максимальная &amp;длина текста</translation>
+        <translation>Максимальна &amp;довжина тексту</translation>
     </message>
     <message>
         <location filename="../config.ui" line="77"/>
         <source>Custom &amp;font size</source>
-        <translation>Пользовательский размер шрифт&amp;а</translation>
+        <translation>Користувацький розмір шри&amp;фту</translation>
     </message>
     <message>
         <location filename="../config.ui" line="87"/>
@@ -44,19 +44,19 @@
         <translation> pt</translation>
     </message>
     <message>
+        <location filename="../config.ui" line="103"/>
+        <source>Filters:</source>
+        <translation>Фільтри:</translation>
+    </message>
+    <message>
         <location filename="../config.ui" line="109"/>
         <source>Filter by &amp;desktop</source>
-        <translation>Фильтр по рабочим столам</translation>
+        <translation>Фільтр по &amp;стільницям</translation>
     </message>
     <message>
         <location filename="../config.ui" line="116"/>
         <source>Filter by s&amp;creen</source>
-        <translation>Фильтр по экранам</translation>
-    </message>
-    <message>
-        <location filename="../config.ui" line="103"/>
-        <source>Filters:</source>
-        <translation>Фильтры:</translation>
+        <translation>Фільтр по &amp;екранам</translation>
     </message>
 </context>
 </TS>
---
+++ translations/lxqt-appswitcher_uk.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
+<context>
+    <name>AppSwitcher</name>
+    <message>
+        <location filename="../src/app-switcher.cpp" line="21"/>
+        <source>Switch applications</source>
+        <translation>Перемикання застосунків</translation>
+    </message>
+    <message>
+        <location filename="../src/app-switcher.cpp" line="22"/>
+        <source>Reverse switch applications</source>
+        <translation>Протилежне перемикання застосунків</translation>
+    </message>
+</context>
+</TS>
