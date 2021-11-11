4fd5261eba6cc36971e3494bbfd575852a3c061b
p-bo <pavel.borecki@gmail.com>
2021-11-11 14:08:24 +0100

    Weblate commits
    Co-authored-by: p-bo <pavel.borecki@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswichter-configuration/cs/
    Translation: zjes/lxqt-appswitcher-configuration

--- config/translations/lxqt-config-appswitcher_cs.ts
+++ config/translations/lxqt-config-appswitcher_cs.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../config.ui" line="14"/>
         <source>LXQt Application Switcher Configuration</source>
-        <translation>LXQt nastavení přepínání aplikací</translation>
+        <translation>Nastavení přepínání aplikací v LXQt</translation>
     </message>
     <message>
         <location filename="../config.ui" line="20"/>
@@ -16,7 +16,7 @@
     <message>
         <location filename="../config.ui" line="26"/>
         <source>Icons &amp;size</source>
-        <translation>Velikost &amp;ikon</translation>
+        <translation>Veliko&amp;st ikon</translation>
     </message>
     <message>
         <location filename="../config.ui" line="36"/>
@@ -26,17 +26,17 @@
     <message>
         <location filename="../config.ui" line="43"/>
         <source>Max &amp;items to show</source>
-        <translation>Nejvíce zobrazených &amp;položek</translation>
+        <translation>Nejvýše zobrazených &amp;položek</translation>
     </message>
     <message>
         <location filename="../config.ui" line="60"/>
         <source>Max text &amp;length</source>
-        <translation>Největší &amp;délka textu</translation>
+        <translation>Největší dé&amp;lka textu</translation>
     </message>
     <message>
         <location filename="../config.ui" line="77"/>
         <source>Custom &amp;font size</source>
-        <translation>Velikost &amp;písma</translation>
+        <translation>Uživatelsky určená velikost &amp;písma</translation>
     </message>
     <message>
         <location filename="../config.ui" line="87"/>
@@ -46,7 +46,7 @@
     <message>
         <location filename="../config.ui" line="109"/>
         <source>Filter by &amp;desktop</source>
-        <translation>Filtrovat podle &amp;plochy</translation>
+        <translation>Filtrovat po&amp;dle plochy</translation>
     </message>
     <message>
         <location filename="../config.ui" line="116"/>
