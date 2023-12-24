334cb113e69e54bc932281249ab7cbc8076bb9be
Sabri Ünal <libreajans@gmail.com>
2023-12-24 22:12:27 +0100

    Weblate commits
    Co-authored-by: Sabri Ünal <libreajans@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswichter-configuration/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswitcher/tr/
    Translation: zjes/lxqt-appswitcher
    Translation: zjes/lxqt-appswitcher-configuration

---
+++ config/translations/lxqt-config-appswitcher_tr.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>Config</name>
+    <message>
+        <location filename="../config.ui" line="14"/>
+        <source>LXQt Application Switcher Configuration</source>
+        <translation>LXQt Uygulama Değiştirici Yapılandırması</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="20"/>
+        <source>Appearance:</source>
+        <translation>Görünüm:</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="26"/>
+        <source>Icons &amp;size</source>
+        <translation>Simge &amp;boyutu</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="36"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="43"/>
+        <source>Max &amp;items to show</source>
+        <translation>Gösterilecek &amp;azami öge</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="60"/>
+        <source>Max text &amp;length</source>
+        <translation>Azami metin &amp;uzunluğu</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="77"/>
+        <source>Custom &amp;font size</source>
+        <translation>Özel &amp;yazıtipi boyutu</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="87"/>
+        <source> pt</source>
+        <translation> pt</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="103"/>
+        <source>Filters:</source>
+        <translation>Süzgeçler:</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="109"/>
+        <source>Filter by &amp;desktop</source>
+        <translation>&amp;Masaüstüne göre süz</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="116"/>
+        <source>Filter by s&amp;creen</source>
+        <translation>&amp;Ekrana göre süz</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/lxqt-appswitcher_tr.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>AppSwitcher</name>
+    <message>
+        <location filename="../src/app-switcher.cpp" line="21"/>
+        <source>Switch applications</source>
+        <translation>Uygulama değiştir</translation>
+    </message>
+    <message>
+        <location filename="../src/app-switcher.cpp" line="22"/>
+        <source>Reverse switch applications</source>
+        <translation>Uygulamaları ters çevir</translation>
+    </message>
+</context>
+</TS>
