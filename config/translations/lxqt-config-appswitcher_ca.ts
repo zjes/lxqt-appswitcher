98f5b15f24898a8d7a2e63dadd0ac0adcd1a1b77
Joan Miró <miro.oro.jm@gmail.com>
2023-12-24 22:12:26 +0100

    Weblate commits
    Co-authored-by: Joan Miró <miro.oro.jm@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswichter-configuration/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/zjes/lxqt-appswitcher/ca/
    Translation: zjes/lxqt-appswitcher
    Translation: zjes/lxqt-appswitcher-configuration

---
+++ config/translations/lxqt-config-appswitcher_ca.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>Config</name>
+    <message>
+        <location filename="../config.ui" line="14"/>
+        <source>LXQt Application Switcher Configuration</source>
+        <translation>Configuració del commutador d&apos;aplicacions LXQt</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="20"/>
+        <source>Appearance:</source>
+        <translation>Aparença:</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="26"/>
+        <source>Icons &amp;size</source>
+        <translation>Mida de&amp; les icones</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="36"/>
+        <source> px</source>
+        <translation> píxels</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="43"/>
+        <source>Max &amp;items to show</source>
+        <translation>Màxim &amp;d&apos;elements a mostrar</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="60"/>
+        <source>Max text &amp;length</source>
+        <translation>&amp;Longitud màxima del text</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="77"/>
+        <source>Custom &amp;font size</source>
+        <translation>Mida del &amp;tipus de lletra personalitzat</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="87"/>
+        <source> pt</source>
+        <translation> pt</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="103"/>
+        <source>Filters:</source>
+        <translation>Filtres:</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="109"/>
+        <source>Filter by &amp;desktop</source>
+        <translation>Filtra per &amp;escriptori</translation>
+    </message>
+    <message>
+        <location filename="../config.ui" line="116"/>
+        <source>Filter by s&amp;creen</source>
+        <translation>Filtra per &amp;pantalla</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/lxqt-appswitcher_ca.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>AppSwitcher</name>
+    <message>
+        <location filename="../src/app-switcher.cpp" line="21"/>
+        <source>Switch applications</source>
+        <translation>Commuta les aplicacions</translation>
+    </message>
+    <message>
+        <location filename="../src/app-switcher.cpp" line="22"/>
+        <source>Reverse switch applications</source>
+        <translation>Inverteix la commutació d&apos;aplicacions</translation>
+    </message>
+</context>
+</TS>
